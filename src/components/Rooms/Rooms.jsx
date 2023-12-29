173d636ceee2a96439c420892782bc5d59db5606
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-12-29 23:39:33 +0600

    Rooms route setup

---
+++ src/components/Rooms/Rooms.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import Card from "./Card";
+
+const Rooms = () => {
+
+    const [rooms, setRooms] = useState([]);
+
+    useEffect(() => {
+        fetch('./rooms.json')
+            .then(res => res.json())
+            .then(data => setRooms(data))
+    }, [])
+
+    return (
+        <div>
+            {
+                rooms?.map(room => (
+                    <Card key={room._id} room={room} />
+                ))
+            }
+        </div>
+    );
+};
+
+export default Rooms;
