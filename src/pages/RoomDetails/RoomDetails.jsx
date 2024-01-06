27f182537a5f3033e1b42ec79503bf74c8fbdb69
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-01-07 01:42:41 +0600

    Date range

--- src/pages/RoomDetails/RoomDetails.jsx
+++ src/pages/RoomDetails/RoomDetails.jsx
@@ -5,6 +5,7 @@ import Container from "../../components/Shared/Container";
 import { Helmet } from "react-helmet-async";
 import Header from "../../components/RoomDetails/Header";
 import RoomInfo from "../../components/RoomDetails/RoomInfo";
+import RoomReservation from "../../components/RoomDetails/RoomReservation";
 
 const RoomDetails = () => {
     const { id } = useParams();
@@ -29,11 +30,16 @@ const RoomDetails = () => {
             <Helmet>
                 <title>{`StayHome | ${room?.title}`}</title>
             </Helmet>
-            <div className="">
+            <div className="max-w-screen-lg mx-auto">
                 <div className="flex flex-col gap-6">
                     <Header room={room} />
-                    <RoomInfo roomData={room} />
-                    {/* Calender */}
+                    <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+                        <RoomInfo roomData={room} />
+                        <div className="md:col-span-3 order-first md:order-last mb-10">
+                            {/* RoomReservation */}
+                            <RoomReservation />
+                        </div>
+                    </div>
                 </div>
             </div>
         </Container>
